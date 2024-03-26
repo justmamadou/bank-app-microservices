c30a5770a517a3f78834a3e42f0f20ed601ef9ce
momodou44 <momodouba44@gmail.com>
2024-03-26 12:31:27 +0000

    change GlobalConfig to a class

--- customer-service/src/main/java/net/mamadou/customerservice/config/GlobalConfig.java
+++ customer-service/src/main/java/net/mamadou/customerservice/config/GlobalConfig.java
@@ -1,9 +1,18 @@
 package net.mamadou.customerservice.config;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @ConfigurationProperties(prefix = "global.params")
-@RefreshScope
-public record GlobalConfig(int p1, int p2) {
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GlobalConfig {
+    private int p1;
+    private int p2;
 }
